907542933213fd0997210698fe9a2f59085f3bad
Moloy Sikka <148451746+MoloySikka@users.noreply.github.com>
2024-06-19 22:24:27 +0530

    latest

--- Moloy/login_signup/login_signup.py
+++ Moloy/login_signup/login_signup.py
@@ -48,24 +48,25 @@ def sign_up(user_name, user_pin):
 
 # sign_up('yelyah', '123456')
 
-def log_in(user_name, user_pin):
-    cursor.execute('select * from user_data')
-    user_data = cursor.fetchall()
-    user_list = [i[1] for i in user_data]
-
-    if user_name in user_list:
-        cursor.execute('select * from user_data order by user_id desc limit 1;')
-        user_id = cursor.fetchall()[0][0] + 1  # Set user id after incrementing from last user
-
-        cursor.execute(f"insert into user_data values({user_id}, '{user_name}', '{user_pin}');")  # add data
-        message = f"""You have signed up to SerenityScope successfully!
-            User ID: {user_id}  (You won't need this)
-            User Name: {user_name}  (Remember this)
-            PIN: {user_pin} (And this)
-            """
-        messagebox.showinfo(title='Signed Up!', message=message)
-
-    else:
-        messagebox.showerror(title='Already Exists!', message=f"A user with the username {user_name} already exists!")
-
-    db.commit()
+# Incomplete
+# def log_in(user_name, user_pin):
+#     cursor.execute('select * from user_data')
+#     user_data = cursor.fetchall()
+#     user_list = [i[1] for i in user_data]
+#
+#     if user_name in user_list:
+#         cursor.execute('select * from user_data order by user_id desc limit 1;')
+#         user_id = cursor.fetchall()[0][0] + 1  # Set user id after incrementing from last user
+#
+#         cursor.execute(f"insert into user_data values({user_id}, '{user_name}', '{user_pin}');")  # add data
+#         message = f"""You have signed up to SerenityScope successfully!
+#             User ID: {user_id}  (You won't need this)
+#             User Name: {user_name}  (Remember this)
+#             PIN: {user_pin} (And this)
+#             """
+#         messagebox.showinfo(title='Signed Up!', message=message)
+#
+#     else:
+#         messagebox.showerror(title='Does Not Exist!', message=f"A user with the username {user_name} does not exist!")
+#
+#     db.commit()
